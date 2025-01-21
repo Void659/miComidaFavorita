f38a50cc8fdd2752f64f904e0f67dc0aac62db4d
Void659 <64670520+Void659@users.noreply.github.com>
2025-01-21 11:58:38 -0400

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 const firebaseConfig = {
 // Pegar configuración de Firebase Console
-apiKey: "AIzaSyDa7KbD96sB8mdGex-SA5fkOYRU7nHQ9Lc",
-  authDomain: "micomidafavorita-5d40b.firebaseapp.com",
-  projectId: "micomidafavorita-5d40b",
-  storageBucket: "micomidafavorita-5d40b.firebasestorage.app",
-  messagingSenderId: "270565778119",
-  appId: "1:270565778119:web:42bcf30c360fbdc5b8d4e2"
+apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 
 };
 const app = initializeApp(firebaseConfig);
